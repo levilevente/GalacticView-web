61259a366780a6d49e17a3085d7f980f48f1a2a4
Daróczi Levente <daroczilevente2@gmail.com>
2025-12-08 20:39:29 +0200

    now if clicked on an item it will be console loged

--- src/components/search/SearchResults.tsx
+++ src/components/search/SearchResults.tsx
@@ -12,6 +12,13 @@ function SearchResults(props: SearchResultsProps) {
     const { results } = props;
     const items: NasaImageAndVideoLibraryItemType[] = results ? getDistinctItemsByTitle(results.collection.items) : [];
 
+    const clickedItem = (item: NasaImageAndVideoLibraryItemType) => {
+        // eslint-disable-next-line no-console
+        console.log(item.data[0]);
+        // eslint-disable-next-line no-console
+        item.links.forEach((link) => console.log(link.href));
+    };
+
     return (
         <div className={style.searchResultsContainer}>
             {items.length === 0 ? (
@@ -19,7 +26,18 @@ function SearchResults(props: SearchResultsProps) {
             ) : (
                 /*Only the top 5 distinct based on title*/
                 items.slice(0, 5).map((item, i) => (
-                    <div className={style.searchResultsItem} key={i}>
+                    <div
+                        className={style.searchResultsItem}
+                        key={`${item.data[0].title}-${i}`}
+                        onClick={() => clickedItem(item)}
+                        role="button"
+                        tabIndex={0}
+                        onKeyDown={(e) => {
+                            if (e.key === 'Enter' || e.key === ' ') {
+                                clickedItem(item);
+                            }
+                        }}
+                    >
                         <p>{item.data[0].title}</p>
                     </div>
                 ))
